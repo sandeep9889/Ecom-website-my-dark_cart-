489c4afaaae294e71013af36c09bf425167abd37
sandeep <sandeepchauhan9630228313@gmail.com>
2021-09-26 12:13:21 +0530

    data base

--- shop/models.py
+++ shop/models.py
@@ -7,6 +7,7 @@ class product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
     desc = models.CharField(max_length=300, blank=True, null=True, )
+     #herer we can put true for blank and null because vo push rha h ki age khali h usko empty rakhana h ki nahi kyu ki mene usko fill nahi kkra tha baad m
     pub_date = models.DateField(null=True,blank=True)
     def __str__(self):
-        return self.product_name
+        return self.product_name #here  we can make tittle as product name
